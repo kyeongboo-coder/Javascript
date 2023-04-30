12088508c904bf7320814b1e78118a6203108c3b
kyeongboo <126529523+kyeongboo-coder@users.noreply.github.com>
2023-04-30 20:56:02 +0900

    Add files via upload

---
+++ javascript100_30,32,33,34.js
@@ -0,0 +1,56 @@
+// 문제 30 : 문자열 속 문자 찾기
+
+// 문자 pineapple에는 apple이라는 문자가 숨어 있습니다. 원범이는 이렇듯 문자열 속에 숨어있는 문자를 찾아보려고 합니다.
+
+// 첫번째 입력에서는 문자열이 입력되고, 두번째에는 찾을 문자가 입력되어야 합니다.
+// **그 문자가 시작하는 index를 반환하는 프로그램**을 만들어 주세요
+
+// 정답과 풀이 :
+
+const word = prompt("입력");
+
+const search = word.indexOf("apple");
+
+console.log(search);
+
+// 문제32 : 문자열 만들기
+
+// 취업 준비생인 혜림이는 자기소개서를 쓰고 있습니다. 열심히 자기소개서를 작성하던 도중 혜림이는 자기가 지금까지 단어를 얼마나 적었는지 궁금하게 됩니다.
+
+// 혜림이를 위해 **문자열을 입력받으면 단어의 갯수를 출력하는 프로그램**을 작성해 주세요.
+
+const word1 = prompt("입력").split(" ");
+
+console.log(word.length);
+
+// 문제33 : 거꾸로 출력하기
+
+// 한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
+
+const n = prompt("입력").split(" ");
+const reverse = n.reverse().join(" ");
+
+console.log(reverse);
+
+// 문제34 :
+// 문제34 : sort 구현하기
+
+// 민주는 체육부장으로 체육시간이 되면 반 친구들이 제대로 키 순서대로 모였는지를 확인해야 한다. 그런데 요즘 민주는 그것이 너무 번거롭게 느껴져 한 번에 확인하고 싶어한다.
+
+// 민주를 위해 **키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램**을 작성해보자.
+// (키는 공백으로 구분하여 입력됩니다.)
+
+const height = prompt("입력");
+
+let started = height
+  .split(" ")
+  .sort(function (a, b) {
+    return a - b;
+  })
+  .join(" ");
+
+if (started === height) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
